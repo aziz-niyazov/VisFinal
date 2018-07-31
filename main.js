df48b7586d2e9cf0d5881168d92901c9b43ef90f
azizniy <azizniy@mail.ru>
2018-07-31 17:04:42 +0200

    players description

--- main.js
+++ main.js
@@ -34,18 +34,16 @@ d3.json("data.json").then(function(data){
 	//iterate through whole json
 	for (var i = 0; i<data.length; i++){
 		
-		//console.log(data[i])
+		console.log(data[i])
 		//check for 'player' key existence
 		if(data[i].hasOwnProperty('player')){
-
-
-			
 			//check for unique IDs -> number of players
 			//add first value
 			if(players.length  == 0 || players === undefined){
-				player1 = new Object();
-				player1.id = data[i].player.id;
+				player1 = new Object()
+				player1.id = data[i].player.id
 				player1.name = data[i].player.name
+				player1.events = new Array
 				players.push(player1);	
 				 
 			}
@@ -59,10 +57,11 @@ d3.json("data.json").then(function(data){
 			}
 			//if not found -> add to the array
 			if(found == false){
-				player1 = new Object();
-				player1.id = data[i].player.id;
+				player1 = new Object()
+				player1.id = data[i].player.id
 				player1.name = data[i].player.name
-				players.push(player1);
+				player1.events = new Array
+				players.push(player1)
 			}
 			
 			
@@ -70,13 +69,80 @@ d3.json("data.json").then(function(data){
 
 	}
 
+	document.write(players.length +"<br>")
+	
+	//adding all the rest to the player object
+	for (var i = 0; i<data.length; i++){
+		if(data[i].hasOwnProperty('player')){
+			for(var j = 0; j < players.length; j++){
+				if(players[j].id == data[i].player.id){
+					//console.log("json " + data[i].timestamp)
+
+					event = new Object()
+					
+
+					type = new Object()
+					type.name = data[i].type.name
+					type.id = data[i].type.id
+					event.type = type
+
+					//need to make data[i].location.x/y
+					loc = new Object()
+					loc.x = data[i].location
+					event.location = loc
+
+					play_pattern = new Object()
+					play_pattern.id = data[i].play_pattern.id
+					play_pattern.name = data[i].play_pattern.name
+					event.play_pattern = play_pattern
+
+
+
+
+					event.timestamp = data[i].timestamp
+					event.index = data[i].index
+					event.id = data[i].id
+					event.minute = data[i].minute
+					event.possession = data[i].possession
+
+
+
+					event.period = data[i].period
+
+					players[j].events.push(event)
+				}
+			}
+			
+
+		}
+	}
+		
+	
+
+
+
 	//player id iteration
+	
 	console.log(players.length)
 	for(var i=0; i<players.length; i++){
-		 document.write(players[i].id + "<br>")
-		 document.write(players[i].name  +"<br>")
+		document.write("player: " + players[i].id + " " + players[i].name + "<br>")
+		for(var j = 0; j<players[i].events.length; j++){
+			document.write("timestamp: " + players[i].events[j].timestamp +"<br>")
+			document.write("id: " + players[i].events[j].id +"<br>")
+			document.write("index: " + players[i].events[j].index +"<br>")
+			document.write("possession: " + players[i].events[j].possession +"<br>")
+			document.write("period: " + players[i].events[j].period +"<br>")
+			document.write("type: " + players[i].events[j].type.id + " " + players[i].events[j].type.name +"<br>")
+			document.write("location: " + players[i].events[j].location.x  +"<br>")
+			document.write("play_pattern: " + players[i].events[j].play_pattern.id + 
+										" " + players[i].events[j].play_pattern.name +"<br>")
+			//document.write("location: " + players[i].events[j].location.x  +"<br>")
 
-		 //console.log(players[i].name)
+
+			document.write("-----------------"  +"<br>")
+		}
+		document.write("=================================================="  +"<br>")
+
 	}
 
 	// players.forEach(function (player){
